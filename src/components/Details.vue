6ad60b3e01a518b17cea7ced904abb3e16191f5a
Mateus <mateus.oliveira@ccc.ufcg.edu.br>
2019-04-28 18:47:41 -0300

    Criando alert básico
    Necessito que revisem e vejam melhores formas de fazer isso, pq queria um negocinho mais bonitinho.

---
+++ src/components/Details.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+  name: 'details',
+  props: ['aula'],
+  mounted (){
+    let detailsAlert = ('Disciplina: '+ this.aula.disciplina+ '\nTurma: '+ this.aula.turma
+                        + '\nCategoria: '+ this.aula.categoria + '\nProfessor: ' + this.aula.professor
+                        + '\nSala: '+ this.aula.sala + '\n')
+    alert(detailsAlert)
+    console.log(this.aula)
+  }
+
+}
+</script>
+
+<style>
+
+</style>

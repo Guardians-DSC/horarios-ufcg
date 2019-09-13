4c21bc49867309b14f864a6a0c0e9291b3713835
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2019-09-13 19:43:10 -0300

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,19 @@
 </template>
 
 <script>
-
+import Navbar from './components/Navbar.vue'
 
 export default {
   name: 'app',
   components: {
-
+    Navbar
   }
 }
 </script>
 
 <style>
 #app {
-  margin: 0px 0px 0px 0px;
-  padding: 0;
+  margin: 0px;
+  padding: 0px;
 }
 </style>

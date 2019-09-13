5464beb312f1a6f9305eefad373907fc07ceb7d8
Douglas Lima <douglas.lima@ccc.ufcg.edu.br>
2019-09-13 18:16:11 -0300

    Firt commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <navbar/>
+  </div>
+</template>
+
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+#app {
+  margin: 0px 0px 0px 0px;
+  padding: 0;
+}
+</style>

d4f3135505f2b1df3061f2c3c6f30dd225217cb4
Jadson Luan <jadsonluan1@gmail.com>
2019-09-15 16:10:24 -0300

    Adiciona modificações iniciais para o componente

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,31 @@
 <template>
   <div id="app">
-    <navbar/>
+    <!-- <navbar/> -->
+    <Horario hora='08'></Horario>
+    <Horario hora='10'></Horario>
+    <Horario hora='14'></Horario>
+    <Horario hora='16'></Horario>
   </div>
 </template>
 
 <script>
 
-
+import Horario from './components/Horario.vue';
 export default {
   name: 'app',
   components: {
-
+    Horario
   }
 }
 </script>
 
 <style>
+html, body { margin: 0; }
 #app {
   margin: 0px 0px 0px 0px;
   padding: 0;
 }
+
+div#app > div.horario:nth-child(even) { background: white; }
+div#app > div.horario:nth-child(odd) { background: lightgrey; }
 </style>

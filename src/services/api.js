5625bdfc949406aff58a6b3cc427bb90646bc8f9
lucaspontoexe <ltx16@hotmail.com>
2019-10-28 00:00:58 -0300

    don't repeat yourself parte 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
 const api = axios.create({
-    baseURL: 'https://horarios-cc-api.herokuapp.com/horarios',
+    baseURL: 'https://horarios-cc-api.herokuapp.com/',
 });
 export default api;

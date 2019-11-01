05a304001d79da31386f1fd6d059f82436681280
breno-pereira <breno.pereira@redealumni.com>
2019-11-01 20:31:43 -0300

    merge master and fix css style

--- src/components/Horario.vue
+++ src/components/Horario.vue
@@ -98,7 +98,7 @@ export default {
     justify-content: center;
   }
 
-  .horario>div{
+  .horario div:first-child{
     display: flex;
     justify-content: space-around;
     align-items: center;

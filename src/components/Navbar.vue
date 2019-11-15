93a48cb182287c312093ea3e2636aac0b8a741e8
Antonio Neto <antonio.bertino.neto@ccc.ufcg.edu.br>
2019-11-15 19:34:07 -0300

    Estilizando a navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,10 @@
         <div id="curso">{{curso}}</div>
         <h1>Horários UFCG</h1>
         <form id="searchBar" v-if="search === true" v-on:submit.prevent="searchActive">
-            <input v-model="searchTerm" placeholder="Disciplina" type="text" list="aulasList" >
+            <div id="searchBarInputContainer">
+                <input id="inputSearch" v-model="searchTerm" placeholder="Pesquisar por Disciplina" type="text" list="aulasList" >
+                <i id="searchIcon" class="fas fa-search"></i>
+            </div>
             <datalist id="aulasList">
                 <option v-for="(aula, index) in getAulas" v-bind:key="index" :value="`${aula.disciplina}-${aula.turma}`"></option>
             </datalist>
@@ -58,6 +61,17 @@ export default {
         z-index: 1;
     }
 
+    #searchIcon {
+        color: #a080c1;
+        margin-right: 20px;
+    }
+
+    #searchBarInputContainer {
+        background-color: #f9f9f9;
+        border-radius: 20px;
+        border: #521782;
+    }
+
     #searchBar {
         display: flex;
         flex-direction: row;
@@ -65,6 +79,18 @@ export default {
         align-items: center;
         justify-self: flex-end;
         margin-right: 20px;
+    }
+
+    #inputSearch {
+        width: 250px;
+        height: 25px;
+        text-align: left;
+        border-radius: 20px;
+        border: #521782;
+        padding-left: 10px;
+        font-size: 15px;
+        font-family: 'Montserrat', sans-serif;
+        color: #521782
     }
     
     .fa-search {

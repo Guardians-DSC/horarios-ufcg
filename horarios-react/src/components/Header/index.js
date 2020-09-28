e2c450a1ae64b89d23dbb483c99862f691c9f22e
Carol Valença <caroliny.leandro@ccc.ufcg.edu.br>
2020-09-28 17:19:59 -0300

    refatoramento de código

--- horarios-react/src/components/Header/index.js
+++ horarios-react/src/components/Header/index.js
@@ -7,15 +7,13 @@ import searchIcon from '../../assets/search.svg'
 function Header() {
     return (
         <div id='header-container'>
-            <div className='header-content'>
-                <div className='course-name'>
-                    <h2>Ciência da Computação</h2>
-                </div>
-                <h1>Horários UFCG</h1>
-                <div className='search-bar'>
-                    <img src={searchIcon} alt=""/>
-                    <input type="text" placeholder='Pesquisar disciplina'/>
-                </div>
+            <div className='course-name'>
+                <h2>Ciência da Computação</h2>
+            </div>
+            <h1>Horários UFCG</h1>
+            <div className='search-bar'>
+                <img src={searchIcon} alt=""/>
+                <input type="text" placeholder='Pesquisar disciplina'/>
             </div>
         </div>
     )

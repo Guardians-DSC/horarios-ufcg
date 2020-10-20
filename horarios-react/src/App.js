2bf49b1239d12f29404ee893a14535190f46beb4
Carol Valença <caroliny.leandro@ccc.ufcg.edu.br>
2020-10-20 00:26:09 -0300

    testando o componente de modal

--- horarios-react/src/App.js
+++ horarios-react/src/App.js
@@ -1,17 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './global.css'
 
 import Header from './components/Header'
 import Filter from './components/Filter'
 import Table from './components/Table'
+import SubjectModal from './components/SubjectModal'
 
 function App() {
+  const [subject, setSubject] = useState()
+  const [classroom, setClassroom] = useState()
+  const [subjectName, setSubjectName] = useState()
+  const [type, setType] = useState()
+  const [period, setPeriod] = useState()
+  const [group, setGroup] = useState()
+  const [modalIsOpen, setIsOpen] = useState(false);
+ 
+  //funcao responsavel por fechar o modal
+  function closeModal(){
+    setIsOpen(false);
+  }
+
+  //funcao responsavel por abrir o modal e receber as informacoes da disciplina necessarias p o modal
+  function openModal(subject, group, subjectName, type, period, classroom) {
+    setIsOpen(true);
+
+    setData(subject, group, subjectName, type, period, classroom)
+  }
+
+  //funcao responsavel por setar os states com as novas informacoes pro modal
+  function setData(subject, group, subjectName, type, period, classroom) {
+    setSubject(subject)
+    setGroup(group)
+    setSubjectName(subjectName)
+    setType(type)
+    setPeriod(period)
+    setClassroom(classroom)
+  }
+
   return (
     <>
       <Header />
       <Filter />
-      <Table />
+      <Table showModal={openModal}/>
+      <SubjectModal 
+        subject={subject}
+        subjectName={subjectName}
+        classroom={classroom}
+        type={type}
+        period={period}
+        group={group}
+        modalIsOpen={modalIsOpen}
+        closeModal={closeModal}
+      />
     </>
   )
 }
